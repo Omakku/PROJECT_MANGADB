37eecf751041e9782eedc9ab9301ad9e44405363
Omakku <luis.silva12321@gmail.com>
2023-12-19 03:07:54 -0500

    exam3
    angularized

--- client/js/view_data.js
+++ client/js/view_data.js
@@ -1,103 +1,170 @@
-main();
+var app = angular.module("viewMangaApp", []);
+app.controller("viewMangaCtrl", function ($scope, $http) {
+  $scope.manga = [];
 
-function main() {
-  retrieveData();
-}
+  //----------------------------------------------------------------
+  //----------------------------------------------------------------
+  $scope.get_records = function () {
+    $http({
+      method: "get",
+      url: "http://localhost:5000" + "/get-records",
+    }).then(
+      function (response) {
+        if (response.data.msg === "SUCCESS") {
+          $scope.manga = response.data.manga;
+          $scope.types = getTypes(response.data.manga);
+          $scope.selectedType = $scope.types[0];
+        } else {
+          console.log(response.data.msg);
+        }
+      },
+      function (response) {
+        console.log(JSON.stringify(response));
+      }
+    );
+  };
+  $scope.get_records();
 
-$("#refresh").click(function () {
-  location.reload();
-  retrieveData();
+  $scope.redrawTable = function () {
+    var type = $scope.selectedType.value;
+
+    $http({
+      method: "get",
+      url: "http://localhost:5000" + "/get-mangaByType",
+      params: { mangaDemographic: type },
+    }).then(
+      function (response) {
+        if (response.data.msg === "SUCCESS") {
+          $scope.manga = response.data.manga;
+        }
+      },
+      function (response) {
+        console.log(response);
+      }
+    );
+  };
+
+  $scope.editManga = function (mangaNumber) {
+    $scope.mangaTitle = $scope.manga[mangaNumber].mangaTitle;
+    $scope.mangaArtist = $scope.manga[mangaNumber].mangaArtist;
+    $scope.mangaDemographic = $scope.manga[mangaNumber].mangaDemographic;
+    $scope.mangaMagazine = $scope.manga[mangaNumber].mangaMagazine;
+    $scope.yearPublished = $scope.manga[mangaNumber].yearPublished;
+    $scope.volumes = $scope.manga[mangaNumber].volumes;
+    $scope.mangaStatus = $scope.manga[mangaNumber].mangaStatus;
+    $scope.mangaGenre = $scope.manga[mangaNumber].mangaGenre;
+    $scope.rating = $scope.manga[mangaNumber].rating;
+    $scope.mangaID = $scope.manga[mangaNumber]["_id"];
+
+    $scope.hideTable = true;
+    $scope.hideForm = false;
+  };
+
+  $scope.cancelUpdate = function () {
+    $scope.hideTable = false;
+    $scope.hideForm = true;
+  };
+
+  //----------------------------------------------------------------
+  $scope.deleteData = function (jsonId) {
+    $http({
+      method: "delete",
+      url: "http://localhost:5000" + "/delete-records",
+      params: { mangaId: jsonId },
+    }).then(
+      function (response) {
+        console.log(response);
+        if (response.data.msg === "SUCCESS") {
+          $scope.redrawTable();
+        } else {
+          console.log(response.data.msg);
+        }
+      },
+      function (response) {
+        console.log(response);
+      }
+    );
+  };
+
+  //----------------------------------------------------------------
+  $scope.updateManga = function () {
+    if (
+      $scope.mangaTitle === "" ||
+      $scope.mangaArtist === "" ||
+      $scope.mangaDemographic === "" ||
+      $scope.mangaMagazine === "" ||
+      $scope.yearPublished === "" ||
+      $scope.volumes === "" ||
+      $scope.mangaStatus === "" ||
+      $scope.mangaGenre === "" ||
+      $scope.rating === ""
+    ) {
+      $scope.addResults = "All Fields are required!";
+      return;
+    }
+
+    $http({
+      method: "put",
+      url: "http://localhost:5000" + "/update-manga",
+      data: {
+        mangaID: $scope.mangaID,
+        mangaTitle: $scope.mangaTitle,
+        mangaArtist: $scope.mangaArtist,
+        mangaDemographic: $scope.mangaDemographic,
+        mangaMagazine: $scope.mangaMagazine,
+        yearPublished: $scope.yearPublished,
+        volumes: $scope.volumes,
+        mangaStatus: $scope.mangaStatus,
+        mangaGenre: $scope.mangaGenre,
+        rating: $scope.rating,
+      },
+    }).then(
+      function (response) {
+        console.log(response);
+        if (response.data.msg === "SUCCESS") {
+          $scope.cancelUpdate();
+          $scope.get_records();
+
+          $scope.mangaTitle = "";
+          $scope.mangaArtist = "";
+          $scope.mangaDemographic = "";
+          $scope.mangaMagazine = "";
+          $scope.yearPublished = "";
+          $scope.volumes = "";
+          $scope.mangaStatus = "";
+          $scope.mangaGenre = "";
+          $scope.rating = "";
+        }
+      },
+      function (response) {
+        console.log(response);
+      }
+    );
+  };
 });
 
-$(".deleteRow-btn").click(function () {
-  var idToDel = $(this).attr("data-id");
-  console.log("DELETE THIS ROW: ");
-  console.log(idToDel);
-  deleteData(idToDel);
-});
-
 //----------------------------------------------------------------
-//----------------------------------------------------------------
-function retrieveData() {
-  $.ajax({
-    url: "http://localhost:5000" + "/get-records",
-    type: "get",
-    success: function (response) {
-      var data = JSON.parse(response);
-      if (data.msg == "SUCCESS") {
-        console.log(data);
-        showTable(data.manga);
-      } else {
-        console.log(data.msg);
-      }
-    },
-    error: function (err) {
-      console.log(err);
-    },
-  });
-}
-//----------------------------------------------------------------
-function deleteData(jsonID) {
-  $.ajax({
-    url: "http://localhost:5000" + "/delete-records",
-    type: "delete",
-    data: { _id: jsonID },
-    success: function (response) {
-      var data = JSON.parse(response);
-      if (data.msg == "SUCCESS") {
-        alert(
-          "ID: " +
-            jsonID +
-            " has been removed!\nPlease refresh table to confirm changes!"
-        );
-        console.log(data);
-      } else {
-        console.log(data.msg);
-      }
-    },
-    error: function (err) {
-      console.log(err);
-    },
-  });
-  location.reload();
-  retrieveData();
-}
 
-//----------------------------------------------------------------
-function showTable(manga) {
-  var htmlString = "";
-  for (var i = 0; i < manga.length; i++) {
-    htmlString += "<tr>";
-    htmlString += "<td>" + manga[i]._id + "</td>";
-    htmlString += "<td>" + manga[i].mangaTitle + "</td>";
-    htmlString += "<td>" + manga[i].mangaArtist + "</td>";
-    htmlString += "<td>" + manga[i].mangaDemographic + "</td>";
-    htmlString += "<td>" + manga[i].mangaMagazine + "</td>";
-    htmlString += "<td>" + manga[i].yearPublished + "</td>";
-    htmlString += "<td>" + manga[i].volumes + "</td>";
-    htmlString += "<td>" + manga[i].mangaStatus + "</td>";
-    htmlString += "<td>" + manga[i].mangaGenre + "</td>";
-    htmlString += "<td>" + manga[i].rating + "</td>";
-    htmlString +=
-      "<td class='deleteColumn' style='display: none'> <button class='deleteRow-btn' data-id='" +
-      manga[i]._id +
-      "'>DELETE</button> </td>";
-    htmlString += "</tr>";
-  }
+function getTypes(mangaTableData) {
+  var typeExists;
 
-  $("#libraryTable").html(htmlString);
+  typesArray = [{ value: "", display: "ALL" }];
 
-  $(".deleteRow-btn").click(function () {
-    var idToDel = $(this).attr("data-id");
-    deleteData(idToDel);
-  });
-
-  $("#edit").click(function () {
-    const deleteRows = document.querySelectorAll(".deleteColumn");
-    deleteRows.forEach((row) => {
-      row.removeAttribute("style");
+  for (var i = 0; i < mangaTableData.length; i++) {
+    typeExists = typesArray.find(function (element) {
+      return element.value === mangaTableData[i].mangaDemographic;
     });
-  });
+
+    if (typeExists) {
+      continue;
+    } else {
+      typesArray.push({
+        value: mangaTableData[i].mangaDemographic,
+        display: mangaTableData[i].mangaDemographic.toUpperCase(),
+      });
+    }
+  }
+  return typesArray;
 }
 
 //----------------------------------------------------------------
--- server/services.js
+++ server/services.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
@@ -9,10 +8,8 @@ var dbURL = "mongodb://127.0.0.1";
 var services = function (app) {
   app.post("/write-record", function (req, res) {
     console.log("write record");
-    var mangaID = Date.now();
 
     var bookData = {
-      mangaID: mangaID,
       mangaTitle: req.body.mangaTitle,
       mangaArtist: req.body.mangaArtist,
       mangaDemographic: req.body.mangaDemographic,
@@ -25,109 +22,209 @@ var services = function (app) {
     };
 
     console.log(JSON.stringify({ bookData }));
-    MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
-      if (err) {
-        return res.status(201).send(JSON.stringify({ msg: err }));
-      } else {
-        var dbo = client.db("mangadb");
+    MongoClient.connect(
+      dbURL,
+      { useUnifiedTopology: true },
+      function (err, client) {
+        if (err) {
+          return res.status(201).send(JSON.stringify({ msg: err }));
+        } else {
+          var dbo = client.db("mangadb");
 
-        dbo.collection("manga").insertOne(bookData, function (err) {
-          if (err) {
-            return res.status(201).send(JSON.stringify({ msg: err }));
-          } else {
-            console.log(bookData);
-            return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
-          }
-        });
-      }
-    });
-  });
-
-  app.get("/get-records", function (req, res) {
-    MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
-      if (err) {
-        return res.status(201).send(JSON.stringify({ msg: err }));
-      } else {
-        var dbo = client.db("mangadb");
-
-        dbo
-          .collection("manga")
-          .find()
-          .toArray(function (err, data) {
+          dbo.collection("manga").insertOne(bookData, function (err) {
             if (err) {
               return res.status(201).send(JSON.stringify({ msg: err }));
             } else {
-              console.log(data);
-              return res
-                .status(200)
-                .send(JSON.stringify({ msg: "SUCCESS", manga: data }));
+              console.log(bookData);
+              return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
             }
           });
+        }
       }
-    });
+    );
+  });
+
+  app.get("/get-records", function (req, res) {
+    MongoClient.connect(
+      dbURL,
+      { useUnifiedTopology: true },
+      function (err, client) {
+        if (err) {
+          return res.status(201).send(JSON.stringify({ msg: err }));
+        } else {
+          var dbo = client.db("mangadb");
+
+          dbo
+            .collection("manga")
+            .find()
+            .toArray(function (err, data) {
+              if (err) {
+                return res.status(201).send(JSON.stringify({ msg: err }));
+              } else {
+                console.log(data);
+                return res
+                  .status(200)
+                  .send(JSON.stringify({ msg: "SUCCESS", manga: data }));
+              }
+            });
+        }
+      }
+    );
+  });
+
+  app.get("/get-mangaByType", function (req, res) {
+    var mangaDemographic = req.query.mangaDemographic;
+    var search =
+      mangaDemographic === "" ? {} : { mangaDemographic: mangaDemographic };
+
+    MongoClient.connect(
+      dbURL,
+      { useUnifiedTopology: true },
+      function (err, client) {
+        if (err) {
+          return res.status(201).send(JSON.stringify({ msg: err }));
+        } else {
+          var dbo = client.db("mangadb");
+
+          dbo
+            .collection("manga")
+            .find(search)
+            .toArray(function (err, data) {
+              if (err) {
+                return res.status(201).send(JSON.stringify({ msg: err }));
+              } else {
+                console.log(data);
+                return res
+                  .status(200)
+                  .send(JSON.stringify({ msg: "SUCCESS", manga: data }));
+              }
+            });
+        }
+      }
+    );
   });
 
   app.delete("/delete-records", function (req, res) {
-    var idToDel = req.body._id;
-
+    var idToDel = req.query.mangaId;
     var s_id = new ObjectId(idToDel);
     var search = { _id: s_id };
 
-    MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
-      if (err) {
-        return res.status(201).send(JSON.stringify({ msg: err }));
-      } else {
-        var dbo = client.db("mangadb");
+    MongoClient.connect(
+      dbURL,
+      { useUnifiedTopology: true },
+      function (err, client) {
+        if (err) {
+          return res.status(201).send(JSON.stringify({ msg: err }));
+        } else {
+          var dbo = client.db("mangadb");
 
-        dbo.collection("manga").deleteOne(search, function (err) {
-          if (err) {
-            return res.status(201).send(JSON.stringify({ msg: err }));
-          } else {
-            return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
-          }
-        });
+          dbo.collection("manga").deleteOne(search, function (err) {
+            if (err) {
+              return res.status(201).send(JSON.stringify({ msg: err }));
+            } else {
+              return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+            }
+          });
+        }
       }
-    });
+    );
+  });
+
+  app.put("/update-manga", function (req, res) {
+    var mangaID = req.body.mangaID;
+    var mangaTitle = req.body.mangaTitle;
+    var mangaArtist = req.body.mangaArtist;
+    var mangaDemographic = req.body.mangaDemographic;
+    var mangaMagazine = req.body.mangaMagazine;
+    var yearPublished = req.body.yearPublished;
+    var volumes = req.body.volumes;
+    var mangaStatus = req.body.mangaStatus;
+    var mangaGenre = req.body.mangaGenre;
+    var rating = req.body.rating;
+
+    var s_id = new ObjectId(mangaID);
+
+    var search = { _id: s_id };
+
+    var updateData = {
+      $set: {
+        mangaTitle: mangaTitle,
+        mangaArtist: mangaArtist,
+        mangaDemographic: mangaDemographic,
+        mangaMagazine: mangaMagazine,
+        yearPublished: yearPublished,
+        volumes: volumes,
+        mangaStatus: mangaStatus,
+        mangaGenre: mangaGenre,
+        rating: rating,
+      },
+    };
+
+    MongoClient.connect(
+      dbURL,
+      { useUnifiedTopology: true },
+      function (err, client) {
+        if (err) {
+          return res.status(201).send(JSON.stringify({ msg: err }));
+        } else {
+          var dbo = client.db("mangadb");
+
+          dbo.collection("manga").updateOne(search, updateData, function (err) {
+            if (err) {
+              return res.status(201).send(JSON.stringify({ msg: err }));
+            } else {
+              console.log("SEARCH" + JSON.stringify(search));
+              console.log("UPDATEDATA: " + JSON.stringify(updateData));
+              return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+            }
+          });
+        }
+      }
+    );
   });
 };
 
 //To Initialize the manga table
 var initializeDatabase = function () {
-  MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      var dbo = client.db("mangadb");
+  MongoClient.connect(
+    dbURL,
+    { useUnifiedTopology: true },
+    function (err, client) {
+      if (err) {
+        console.log(err);
+      } else {
+        var dbo = client.db("mangadb");
 
-      //See if the database has any records
-      dbo
-        .collection("manga")
-        .find()
-        .toArray(function (err, data) {
-          if (err) {
-            client.close();
-            console.log(err);
-          } else {
-            if (data.length === 0) {
-              var manga = dbFile;
-
-              dbo.collection("manga").insertMany(manga, function (err) {
-                if (err) {
-                  client.close();
-                  console.log(err);
-                } else {
-                  console.log("Added seed records");
-                  client.close();
-                }
-              });
-            } else {
-              console.log("Seed record already exist");
+        //See if the database has any records
+        dbo
+          .collection("manga")
+          .find()
+          .toArray(function (err, data) {
+            if (err) {
               client.close();
+              console.log(err);
+            } else {
+              if (data.length === 0) {
+                var manga = dbFile;
+
+                dbo.collection("manga").insertMany(manga, function (err) {
+                  if (err) {
+                    client.close();
+                    console.log(err);
+                  } else {
+                    console.log("Added seed records");
+                    client.close();
+                  }
+                });
+              } else {
+                console.log("Seed record already exist");
+                client.close();
+              }
             }
-          }
-        });
+          });
+      }
     }
-  });
+  );
 };
 
 module.exports = { services, initializeDatabase };
